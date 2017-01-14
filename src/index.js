e0c6e0616a5ff73e98c8fc67d47cc9aa6dd5bb2e
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-14 19:28:43 +1100

    add initial tweet component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var Message = React.createClass({
+    render: function() {
+        return (
+            <div className="message">
+                This is less than 140 characters.
+            </div>
+        );
+    }
+});
+var NameWithHandle = React.createClass({
+    render: function() {
+        return (
+            <span className="name-with-handle">
+<span className="name">Your Name</span>
+<span className="handle">@yourhandle</span>
+</span>
+        );
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <img src="https://www.gravatar.com/avatar/nothing"
+                 className="avatar"
+                 alt="avatar" />
+        );
+    }
+});
+
 var Tweet = React.createClass({
-    render: function () {
+    render: function() {
         return (
             <div className="tweet">
-                Tweet
+                <Avatar/>
+                <div className="content">
+                    <NameWithHandle/>
+                    <Message/>
+                </div>
             </div>
         );
     }
