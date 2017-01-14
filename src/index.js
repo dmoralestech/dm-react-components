ffe1152128a555ec8de23a14396816291bd15ab6
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-14 20:40:16 +1100

    converted class to functions

--- src/index.js
+++ src/index.js
@@ -2,92 +2,79 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var Time = React.createClass({
-    render: function () {
-        return (
-            <span className="time">3h ago</span>
-        );
-    }
-});
-var ReplyButton = React.createClass({
-    render: function () {
-        return (
-            <i className="fa fa-reply reply-button"/>
-        );
-    }
-});
-var RetweetButton = React.createClass({
-    render: function () {
-        return (
-            <i className="fa fa-retweet retweet-button"/>
-        );
-    }
-});
-var LikeButton = React.createClass({
-    render: function () {
-        return (
-            <i className="fa fa-heart like-button"/>
-        );
-    }
-});
+var Time = () => {
+    return (
+        <span className="time">3h ago</span>
+    );
+}
 
-var MoreOptionsButton = React.createClass({
-    render: function () {
-        return (
-            <i className="fa fa-ellipsis-h more-options-button"/>
-        );
-    }
-});
+var ReplyButton = () => {
+    return (
+        <i className="fa fa-reply reply-button"/>
+    );
+}
+
+var RetweetButton = () => {
+    return (
+        <i className="fa fa-retweet retweet-button"/>
+    );
+}
+
+var LikeButton = () => {
+    return (
+        <i className="fa fa-heart like-button"/>
+    );
+}
 
 
-var Message = React.createClass({
-    render: function () {
-        return (
-            <div className="message">
-                This is less than 140 characters.
-            </div>
-        );
-    }
-});
-var NameWithHandle = React.createClass({
-    render: function () {
-        return (
-            <span className="name-with-handle">
-<span className="name">Your Name</span>
-<span className="handle">@yourhandle</span>
-</span>
-        );
-    }
-});
+var MoreOptionsButton = () => {
+    return (
+        <i className="fa fa-ellipsis-h more-options-button"/>
+    );
+}
 
-var Avatar = React.createClass({
-    render: function () {
-        return (
-            <img src="https://www.gravatar.com/avatar/nothing"
-                 className="avatar"
-                 alt="avatar"/>
-        );
-    }
-});
 
-var Tweet = React.createClass({
-    render: function () {
-        return (
-            <div className="tweet">
-                <Avatar/>
-                <div className="content">
-                    <NameWithHandle/><Time/>
-                    <Message/>
-                    <div className="buttons">
-                        <ReplyButton/>
-                        <RetweetButton/>
-                        <LikeButton/>
-                        <MoreOptionsButton/>
-                    </div>
+var Message = () => {
+    return (
+        <div className="message">
+            This is less than 140 characters.
+        </div>
+    );
+}
+
+var NameWithHandle = () => {
+    return (
+        <span className="name-with-handle">
+            <span className="name">Your Name</span>
+            <span className="handle">@yourhandle</span>
+        </span>
+    );
+}
+
+var Avatar = () => {
+    return (
+        <img src="https://www.gravatar.com/avatar/nothing"
+             className="avatar"
+             alt="avatar"/>
+    );
+}
+
+var Tweet = () => {
+    return (
+        <div className="tweet">
+            <Avatar/>
+            <div className="content">
+                <NameWithHandle/><Time/>
+                <Message/>
+                <div className="buttons">
+                    <ReplyButton/>
+                    <RetweetButton/>
+                    <LikeButton/>
+                    <MoreOptionsButton/>
                 </div>
             </div>
-        );
-    }
-});
+        </div>
+    );
+}
 
 ReactDOM.render(<Tweet/>, document.querySelector('#root'));
