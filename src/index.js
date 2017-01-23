0100e53f4af57ae8eeccb71c3d427c6fae27d058
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-24 00:43:49 +1100

    React Children API sample

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 // import ProductList from  './ProductList';
 // import CheckOutMain from './checkOut/CheckOutMain'
 // import SignUp from './sign-up/SignUp'
-import FunctionAsComponent from './functionAsComponent/FunctionAsComponent'
+// import FunctionAsComponent from './functionAsComponent/FunctionAsComponent'
 // import Tweet from './tweet/Tweet'
+import App from './reactChildren/App'
 
 // const MainBody = () => {
 //     return (
@@ -16,4 +17,4 @@ import FunctionAsComponent from './functionAsComponent/FunctionAsComponent'
 //     );
 // }
 
-ReactDOM.render(<FunctionAsComponent/>, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
