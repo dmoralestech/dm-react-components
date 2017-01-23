0ff471722855d71869b9655c59ed34f3a7859c3c
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-23 22:04:36 +1100

    function as a component sample

---
+++ src/functionAsComponent/FunctionAsComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ScrollPos from './ScrollPos';
+import './App.css';
+
+class FunctionAsComponent extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="spacer" />
+
+                <ScrollPos>
+                    {
+                        position => <h1>{'Awesome Text!!!'.substr(0,position*15)}</h1>
+                    }
+                </ScrollPos>
+
+                <div className="spacer" />
+            </div>
+        );
+    }
+}
+
+export default FunctionAsComponent;
