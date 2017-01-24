a5663b778f74aab4169f4edb0c6bf7360c76a3df
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-24 22:50:38 +1100

    fix missing image

--- src/reactChildren/App.js
+++ src/reactChildren/App.js
@@ -7,9 +7,9 @@ class App extends Component {
         return (
             <div className="App">
                 <SlideShow>
-                    <img src="http://placekitten.com/300/200" alt="Cat Pic" />
                     <img src="http://placekitten.com/300/201" alt="Cat Pic" />
                     <img src="http://placekitten.com/300/202" alt="Cat Pic" />
+                    <img src="http://placekitten.com/300/203" alt="Cat Pic" />
                 </SlideShow>
             </div>
         );
