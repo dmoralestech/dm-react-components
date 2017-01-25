b223a9def836377793fa51e26c254a1774dfbbcf
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-25 21:52:46 +1100

    router sample

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+
 import './index.css';
 // import ProductList from  './ProductList';
 // import CheckOutMain from './checkOut/CheckOutMain'
 // import SignUp from './sign-up/SignUp'
 // import FunctionAsChildComponent from './functionAsComponent/FunctionAsChildComponent'
-// import Tweet from './tweet/Tweet'
-import App from './reactChildren/App'
+import Tweet from './tweet/Tweet'
+// import App from './reactChildren/App'
 import LandingPage from './landing-page/LandingPage'
 
-// const MainBody = () => {
-//     return (
-//         <div>
-//             <Tweet/>
-//             {/*<ProductList/>*/}
-//         </div>
-//     );
-// }
+const Home = () => (<div><h1>Welcome home</h1></div>)
+const About = () => (<div><h1>About</h1></div>)
 
-ReactDOM.render(<LandingPage/>, document.querySelector('#root'));
+export class App extends React.Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component={Home} />
+                <Route path="/landing-page" component={LandingPage} />
+                <Route path="/tweet" component={Tweet} />
+                <Route path="/about" component={About} />
+            </Router>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
