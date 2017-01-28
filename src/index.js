08220911a64eb63cb6c2f4fdfdec5ff157815928
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-28 22:18:47 +1100

    add routing

--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ import './index.css';
 // import CheckOutMain from './checkOut/CheckOutMain'
 // import SignUp from './sign-up/SignUp'
 // import FunctionAsChildComponent from './functionAsComponent/FunctionAsChildComponent'
-import Tweet from './tweet/Tweet'
+// import Tweet from './tweet/Tweet'
 // import App from './reactChildren/App'
-import LandingPage from './landing-page/LandingPage'
+// import LandingPage from './landing-page/LandingPage'
 // import Calculator from './calculator/Calculator'
-import ImageProfilePic from './ImageProfilePic/ImageProfilePic'
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
-import {Index} from './containers/Index';
-import AppRoute from './AppRoute'
+// import ImageProfilePic from './ImageProfilePic/ImageProfilePic'
 
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import AppRoute from './AppRoute'
 
 ReactDOM.render(<Router history={browserHistory} routes={AppRoute}/>,
     document.querySelector('#root'));
