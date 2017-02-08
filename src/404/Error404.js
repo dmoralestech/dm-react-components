37a570f24e9575d3059d9e6edf3ec3b6d47bbe55
darwinmorales <darwin.morales.tech@gmail.com>
2017-02-09 07:01:28 +1100

    add dropdown box

--- src/404/Error404.js
+++ src/404/Error404.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Select from 'react-select'
-
+import Dropdown from 'react-dropdown'
 import './Error404.css'
 
 var options = [
@@ -11,30 +11,38 @@ var options = [
     { value: 'five', label: 'Five' }
 ];
 
+const defaultOption = options[0]
+
 function logChange(val) {
     console.log("Selected: " + val);
 }
 
 
 class Error404  extends React.Component {
-    getDefaultProps() {
+    static defaultProps() {
         return { image: 'http://i.giphy.com/l117HrgEinjIA.gif' }
     }
 
-    renderTest() {
+    render() {
         return (
             <div>
-                <Select
-                    name="form-field-name"
-                    value="one"
-                    options={options}
-                    onChange={logChange}
+                <Dropdown options={options}
+                          onChange={this._onSelect}
+                          value={defaultOption}
+                          placeholder="Select an option"
                 />
+
+                {/*<Select*/}
+                    {/*name="form-field-name"*/}
+                    {/*value="one"*/}
+                    {/*options={options}*/}
+                    {/*onChange={logChange}*/}
+                {/*/>*/}
             </div>
         )
     }
 
-    render() {
+    renderOld() {
         return (
         <div className="FourOhFour">
             <div className="bg" style={{ backgroundImage: 'url(' + this.props.image + ')'}}></div>
