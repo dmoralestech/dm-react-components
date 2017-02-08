87871f093331fe34baf30799d850fabb0ebf56f9
darwinmorales <darwin.morales.tech@gmail.com>
2017-02-09 06:43:23 +1100

    error 404 component

--- src/404/Error404.js
+++ src/404/Error404.js
@@ -1,17 +1,43 @@
 import React from 'react'
+import Select from 'react-select'
 
 import './Error404.css'
+
+var options = [
+    { value: 'one', label: 'One' },
+    { value: 'two', label: 'Two' },
+    { value: 'three', label: 'Three' },
+    { value: 'four', label: 'Four' },
+    { value: 'five', label: 'Five' }
+];
+
+function logChange(val) {
+    console.log("Selected: " + val);
+}
+
 
 var Error404 = React.createClass({
     getDefaultProps: function() {
         return { image: 'http://i.giphy.com/l117HrgEinjIA.gif' }
     },
+    renderTest: function() {
+        return (
+            <div>
+                <Select
+                    name="form-field-name"
+                    value="one"
+                    options={options}
+                    onChange={logChange}
+                />
+            </div>
+        )
+    },
     render: function() {
         return (
-            <div className="FourOhFour">
-                <div className="bg" style={{ backgroundImage: 'url(' + this.props.image + ')'}}></div>
-                <div className="code">404</div>
-            </div>
+        <div className="FourOhFour">
+            <div className="bg" style={{ backgroundImage: 'url(' + this.props.image + ')'}}></div>
+            <div className="code">404</div>
+        </div>
         )
     }
 });
